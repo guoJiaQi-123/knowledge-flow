0e6bf86d9faea85245b5e9904b8c7e834ef76f3f
GuoJiaqi <2667629684@qq.com>
2024-10-22 23:51:13 +0800

    单例模式懒汉式-枚举实现

---
+++ 计算机基础/设计模式/designmode/src/main/java/com/tyut/design/E1_singleton/Singleton7.java
@@ -0,0 +1,27 @@
+package com.tyut.design.E1_singleton;
+
+/**
+ * @version v1.0
+ * @author OldGj 2024/10/22
+ * @apiNote 单例模式-枚举实现-推荐使用
+ */
+public class Singleton7 {
+
+    public static void main(String[] args) {
+        Singleton instance = Singleton.INSTANCE;
+        Singleton instance2 = Singleton.INSTANCE;
+        System.out.println(instance.hashCode());
+        System.out.println(instance2.hashCode());
+    }
+}
+
+
+enum Singleton {
+
+    INSTANCE;
+
+    void doSomething() {
+        // 单例的具体业务逻辑
+    }
+
+}
