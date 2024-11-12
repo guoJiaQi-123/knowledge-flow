31312bbbfd4d1368d0b56bb88c83653924e09b5a
GuoJiaqi <guojiaqi5946@outlook.com>
2024-11-12 21:35:29 +0800

    「新增」Java并发测试类

---
+++ Java/Java并发/code/src/main/java/com/tyut/test/Test1.java
@@ -0,0 +1,42 @@
+package com.tyut.test;
+
+import com.tyut.util.Joiner;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @version v1.0
+ * @author OldGj 2024/11/12
+ * @apiNote TODO(一句话给出该类描述)
+ */
+@Slf4j(topic = "c.Test1")
+public class Test1 {
+
+    static int count = 0;
+    static Object lock = new Object();
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 5000; i++) {
+                // synchronized就是对象锁，只能锁对象，如果synchronized加在成员方法上，表示锁的是this对象
+                // synchronized如果加在静态方法上，表示锁的是类对象
+                synchronized (lock) { // 对象锁
+                    count++;
+                }
+            }
+        }, "t1");
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 5000; i++) {
+                synchronized (lock) {
+                    count--;
+                }
+            }
+        }, "t2");
+
+        t1.start();
+        t2.start();
+        Joiner.join(t1);
+        Joiner.join(t2);
+        System.out.println(count);
+    }
+}
