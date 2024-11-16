fafeb1683559be15158c6d5de175ab4a460b66a0
GuoJiaqi <guojiaqi5946@outlook.com>
2024-11-16 18:03:24 +0800

    「新增」test测试类

---
+++ Java/Java并发/code/src/main/java/com/tyut/test/Test2.java
@@ -0,0 +1,46 @@
+package com.tyut.test;
+
+import com.tyut.util.Sleeper;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @version v1.0
+ * @author OldGj 2024/11/16
+ * @apiNote TODO(一句话给出该类描述)
+ */
+@Slf4j(topic = "c.Test2")
+public class Test2 {
+    public static final Object lock = new Object();
+
+    @SuppressWarnings("all")
+    public static void main(String[] args) {
+        new Thread(() -> {
+            log.debug("执行");
+            synchronized (lock) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            log.debug("{}执行其他操作", Thread.currentThread().getName());
+        }, "t1").start();
+
+        new Thread(() -> {
+            log.debug("执行");
+            synchronized (lock) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            log.debug("执行其他操作{}", Thread.currentThread().getName());
+        }, "t2").start();
+
+        Sleeper.sleep(2);
+        synchronized (lock) {
+            lock.notify();
+        }
+    }
+}
