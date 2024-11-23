d49a7a543d4116d9e7573da26b9f92cf4fdb68fb
GuoJiaqi <guojiaqi5946@outlook.com>
2024-11-23 21:47:46 +0800

    「新增」白箱备忘录模式

---
+++ 计算机基础/设计模式/designmode/src/main/java/com/tyut/design/E19_memento/write_memento/Main.java
@@ -0,0 +1,30 @@
+package com.tyut.design.E19_memento.write_memento;
+
+/**
+ * @version v1.0
+ * @author OldGj 2024/11/23
+ * @apiNote Main方法 -> 白箱备忘录模式
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("------------大战Boss前------------");
+        //大战Boss前
+        GameRole gameRole = new GameRole();
+        gameRole.initState();
+        gameRole.stateDisplay();
+
+        //保存进度
+        RoleStateCaretaker roleStateCaretaker = new RoleStateCaretaker();
+        roleStateCaretaker.setRoleStateMemento(gameRole.saveState());
+
+        System.out.println("------------大战Boss后------------");
+        //大战Boss时，损耗严重
+        gameRole.fight();
+        gameRole.stateDisplay();
+        System.out.println("------------恢复之前状态------------");
+        //恢复之前状态
+        gameRole.recoverState(roleStateCaretaker.getRoleStateMemento());
+        gameRole.stateDisplay();
+
+    }
+}
---
+++ 计算机基础/设计模式/designmode/src/main/java/com/tyut/design/E19_memento/write_memento/RoleStateCaretaker.java
@@ -0,0 +1,16 @@
+package com.tyut.design.E19_memento.write_memento;
+
+/**
+ * 角色状态管理者类
+ */
+public class RoleStateCaretaker {
+    private RoleStateMemento roleStateMemento;
+
+    public RoleStateMemento getRoleStateMemento() {
+        return roleStateMemento;
+    }
+
+    public void setRoleStateMemento(RoleStateMemento roleStateMemento) {
+        this.roleStateMemento = roleStateMemento;
+    }
+}
